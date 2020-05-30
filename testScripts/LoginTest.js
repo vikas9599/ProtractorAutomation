425c28fa8218d0e76383e27011ac7575cb05b7f0
Vikas Rathour <vikas.rathour@videotap.com>
2020-05-30 07:40:30 +0530

    chnages in report

--- testScripts/LoginTest.js
+++ testScripts/LoginTest.js
@@ -1,29 +1,26 @@
 var lg = require("../utility/LoginLib");
 var util = require("../utility/Utility");
-var URL = "https://qadr.videotap.com:3011/#!/";
+var URL = "https://www.google.com:3011/#!/";
 
 describe("Website Should Open", () => {
-  it("Open Webiste URL", async function() {
+  it("Open Webiste URL", async function () {
     lg.OpenWebsite(URL);
     lg.MaxmizeScreen();
 
     var title = await browser.getTitle();
-    var expected = "VideoTap Design Room";
+    var expected = "Expected title of web";
     util.VerifyTitle(title, expected);
   });
 
-  it("User Should be Enter Username & Password", async function() {
-    lg.UserLogin("vikas@videotap.com", "zxcvbnm");
+  it("User Should be Enter Username & Password", async function () {
+    lg.UserLogin("vikas@gmail.com", "zxcvbnm");
   });
 
-  it("User Should be Logged IN ", async function() {
+  it("User Should be Logged IN ", async function () {
     lg.ClickLogin();
     util.addWait(5000);
     var title = await browser.getTitle();
-    var expected = "VideoTap Design";
+    var expected = "Expected Title";
     util.VerifyTitle(title, expected);
   });
-
-it('' , )
-
 });
